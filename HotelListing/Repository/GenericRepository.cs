5bcf97f2876b5dace7a3f6536d67e2cbb30e5f2d
hosseinsadeghikia <49323084+hosseinsadeghikia@users.noreply.github.com>
2022-11-02 09:34:56 +0330

    Setup Service Repositories and Dependency Injection

---
+++ HotelListing/Repository/GenericRepository.cs
@@ -0,0 +1,101 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using HotelListing.Data;
+using HotelListing.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly  ApplicationDbContext _context;
+        private readonly  DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IList<T>> GetAll(Expression<Func<T, bool>>? expression = null, Func<IQueryable<T>,
+            IOrderedQueryable<T>>? orderBy = null, List<string>? includes = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>>? expression,
+            List<string>? includes = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task InsertRange(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null) _dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void UpdateRange(IEnumerable<T> entities)
+        {
+            var enumerable = entities as T[] ?? entities.ToArray();
+            _dbSet.AttachRange(enumerable);
+            _context.Entry(enumerable).State = EntityState.Modified;
+        }
+    }
+}
